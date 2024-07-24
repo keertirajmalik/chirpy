4c8aed2e004040387b037cac61c9a0525a7679a9
KeertirajMalik <keertirajlaxman.malik@imtf.com>
2024-07-24 20:10:28 +0530

    feat: api with json response

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
-
-func handlerReadiness(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte("OK"))
-}
 
 func main() {
 	const port = "8080"
@@ -28,6 +23,8 @@ func main() {
 
 	mux.HandleFunc("GET /api/reset", metrics.handleReset)
 
+	mux.HandleFunc("POST /api/validate_chirp", handleValidateChirp)
+
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -35,5 +32,64 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
 
+func handlerReadiness(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("OK"))
+}
+
+func handleValidateChirp(writer http.ResponseWriter, request *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	type validResponse struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(request.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		writer.WriteHeader(500)
+		return
+	}
+
+	if len(params.Body) > 140 {
+
+		response := errorResponse{
+			Error: "Chirp is too long",
+		}
+		dat, err := json.Marshal(response)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			writer.WriteHeader(500)
+			return
+		}
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(400)
+		writer.Write(dat)
+		return
+	}
+
+	response := validResponse{
+		Valid: true,
+	}
+	dat, err := json.Marshal(response)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		writer.WriteHeader(500)
+		return
+	}
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(200)
+	writer.Write(dat)
+	return
 }
